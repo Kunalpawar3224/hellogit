fec81a055637c4abcd11696ee45ca7dc6b7e8366
Kunal Pawar <kpawar3224@gmail.com>
2022-12-02 18:03:55 +0530

    added comments and make some corrections

--- bloom_filter3.py
+++ bloom_filter3.py
@@ -1,29 +1,41 @@
-class bloom_filter :
-    '''
-    Bloom filter implementation
-    '''
-    array =  [True,True,True,True,True,True,True,True,True]
-    k=0
-    train_list = [] 
-    train = ['cat', 'dog', 'parrot']
-    user=['monkey', 'dog']
+#def  bloom_filter():
+'''
+ Bloom filter implementation of simple logic for bloom filter problem statement
+ http://codekata.com/kata/kata05-bloom-filters/
+'''
+array =  [True,True,True,True,True,True,True,True,True]
+k=0
+train_list = [] 
+train = ['cat', 'dog', 'parrot']
+#Elements which are stored 
 
-    def train_(self,array,train) :
-            
-        for i in train:
-            print(i)
-            self.train_list.append(len(i))
-            self.array = array
-            
-            if array[len(i)] == True:
-                array[len(i)]=False
-            elif array[len(i)] != True :   
-                array[len(i)]=False
-        #print(array)
-        return array
+user=['monkey', 'cat', 'mouse']
+#Elements to check which are present or not
 
-    result =train([True,True,True,True,True,True,True,True,True], ['cat','dog','parrot'])
-    #print(array)
-    print(result)
-    #print(array)
+def  bloom_filter():
+ '''bloom filter implementation'''
+ for i in train:
+   # print(i)
+    train_list.append(len(i))
+       
+    if array[len(i)] == True:
+        array[len(i)]=False
+    elif array[len(i)] != True :   
+        array[len(i)]=False
+ print(train_list)
+ return array
 
+result = bloom_filter()
+#print(array)
+print(result)
+
+def user_once():
+ for i in user:
+   #print(len(i))
+    if array[len(i)]== True:
+        print(i,': definately not present')
+    elif array[len(i)]== False:
+        print(i,': may be present')
+
+result1 = user_once()
+#print(result1)
