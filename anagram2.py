6682787d44f83618db06a2419582d4bc40017294
Kunal <kpawar3224@gmail.com>
2022-12-06 23:55:31 +0530

    use dictionary to print set of anagram

--- anagram2.py
+++ anagram2.py
@@ -2,34 +2,21 @@ def anagram():
     '''
     Implementation of Anagram problem statement
     '''
-    str1_list =[]
-    str2_list =[]
+    anagrams={}
+    num = 0
     
-    f= open("/home/kunal/anagram.txt", "r")
-    
-    a=f.readline()
-
-    while a:
-        
-        a1 = sorted(a)
-
-        with open ("/home/kunal/anagram.txt") as file:
-            for i in file :
-
-                i1 = sorted(i)
+    with open ("C:\\Users\\Dell\\Documents\\wordlist.txt") as file:
+        for i in file :
+            i = i.strip()
+            key = "".join(sorted(i))
                  
-                if len(a) == len(i):
+            if key in anagrams:
+                anagrams[key].append(i)
                     #Look for the possible anagrams from the strings which has equal lengths
+            else :
+                anagrams[key] = [i]
 
-                    if a==i :
-                        continue
-                        #skip the given string, because it occurs two times in the output
-
-                    elif a1 == i1 :
-                        #Confirmation of the anagram, by comparing sorted string
-                        str1_list.append(i.strip())                        
-                        print(a.strip(), str1_list)      
-                str1_list.clear()           
-        a=f.readline()                      
-                                        
+            num +=1                    
+        print(anagrams)
+        print(num)                                
 anagram()
