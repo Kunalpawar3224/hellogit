f0a33e31e9d4368f7b13c5940c9b5f5185a41b61
Kunal <kpawar3224@gmail.com>
2023-02-25 14:02:02 +0530

    changed the ui of admin

--- django_learning/Hello/Hello/urls.py
+++ django_learning/Hello/Hello/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Kunal Provision Store Admin"
+admin.site.site_title = "UMSRA Admin Portal"
+admin.site.index_title = "Welcome to Kunal Provision Store"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
