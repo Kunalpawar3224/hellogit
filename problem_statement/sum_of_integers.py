8cb50e04b16be8aaa0e4f1fa5c5ce733854c00cb
Kunal <kpawar3224@gmail.com>
2023-05-14 13:04:50 +0530

    ncjefnvjfv

--- problem_statement/sum_of_integers.py
+++ problem_statement/sum_of_integers.py
@@ -1,24 +1,28 @@
 # Referencece: https://realpython.com/python-practice-problems/#problem-description
 
-# def add_it_up():
-#     num = int(input("Enter youur number: "))
-#     sum = 0
-#     try:
-#         for i in range(0 ,num+1):
-#             sum += i   
-#     except:
-#         sum = 0 
-#     print(sum)
-#     return sum 
-
-# add_it_up()
-
-def add_it_up(n):
+def add_it_up():
+    
     
     try:
-        result = sum(range(n + 1))
+        num = int(input("Enter youur number: "))
+        # sum = 0
+        result = sum(range(num + 1))
+        # for i in range(0 ,num+1):
+        #     sum += i   
+        # print(sum)    
     except TypeError:
-        result = 0
+        result = 0  
     print(result)    
     return result
-add_it_up(4)
+
+add_it_up()
+
+# def add_it_up(n):
+    
+#     try:
+#         result = sum(range(n + 1))
+#     except TypeError:
+#         result = 0
+#     print(result)    
+#     return result
+# add_it_up(9)
