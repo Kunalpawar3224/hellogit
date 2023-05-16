60b41a36f1ec5379951983915a3772ffc7f1c620
Kunal <kpawar3224@gmail.com>
2023-05-16 17:26:28 +0530

    first impelmentation need to improve the code

--- problem_statement/caesar_cipher.py
+++ problem_statement/caesar_cipher.py
@@ -1,8 +1,19 @@
 # reference: https://realpython.com/python-practice-problems/
 
-from caesarcipher import CaesarCipher
+# from caesarcipher import CaesarCipher
 
-cipher = CaesarCipher('abcd xyz',
-  offset=4)
+# cipher = CaesarCipher('abcd xyz',
+#   offset=4)
 
-print(cipher.encoded)
+# print(cipher.encoded)
+
+def caesar(str , offset):
+
+    for i in str:
+        # need to refactor the code
+        char = chr(ord(i)+offset)
+        print(char, end='')
+
+caesar("abcd xyz", 4)        
+
+
