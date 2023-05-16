55987b6ddfdf23c2d2d01f55ccba2c17b527e7db
Kunal <kpawar3224@gmail.com>
2023-05-16 11:43:46 +0530

    better implemantion than previous

--- problem_statement/credit_card.py
+++ problem_statement/credit_card.py
@@ -1,16 +1,22 @@
 #reference: https://www.codecademy.com/resources/blog/python-code-challenges-for-beginners/
 
 def credit_card(card):
-    if len(card) == 16:
-        print("it is a credit card")
+
+    if  len(card) == 16:
 
         for i in card[:12]:
-                
+           
+            if i.isdigit():
             #if i <= 8:
                 i = '*'
                 print(i, end='')   
+                
+                #break
+            else:
+                print("It is not a credit")    
         print(card[12:16])
     else:
         print("it is a not credit card")    
 
-credit_card("4444444444444444")        
+credit_card("123e567891234567") 
+
